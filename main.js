6d358fe4ca571be7eb0d1c587dbbf055ea486b79
Radvir <kovacstihamer500@gmail.com>
2022-11-11 20:43:00 +0100

    hello command

---
+++ main.js
@@ -0,0 +1,29 @@
+const { token, prefix } = require('./config.json');
+const discord = require('discord.js');
+const client = new discord.Client({intents: new discord.Intents(32767)});
+
+client.on('ready', () => {
+    console.log(client.user.tag + " elindult!")
+
+    let commands = client.application.commands;
+    commands.create({
+        name : "hello",
+        description: "welcoming message"
+    });
+})
+
+client.on('interactionCreate', interaction => {
+    if(!interaction.isCommand) return;
+    let name = interaction.commandName;
+    let optins = interaction.optins;
+
+    if (name == "hello"){
+        interaction.reply({
+            content: "hello",
+            ephemeral: true
+        })
+    }
+})
+
+
+client.login(token);
